c20e6e21ba53d9e8e3cff5489c27f7220c7ee3ab
Sander <sander@millerdigital.nl>
2024-02-06 00:18:23 +0100

    Add font

--- init.zsh
+++ init.zsh
@@ -4,6 +4,9 @@ xargs brew install < brew_leaves
 # Symlink to home directory
 stow --adopt .
 
+# Install Jetbrains font
+cp ./JetBrainsMonoNerdFont-Regular.ttf ~/Library/Fonts/JetBrainsMonoNerdFont-Regular.ttf 
+
 # Link iterm2 config
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
