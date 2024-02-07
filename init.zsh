7ec4e0ca818690459a480bbe8112be0718ea7996
Sander Meij <sander@millerdigital.nl>
2024-02-07 22:30:54 +0100

    Fix fonts

--- init.zsh
+++ init.zsh
@@ -4,11 +4,14 @@ xargs brew install < brew_leaves
 # Install TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Install Zap
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
 # Symlink to home directory
 stow --adopt .
 
 # Install Jetbrains font
-cp ./JetBrainsMonoNerdFont-Regular.ttf ~/Library/Fonts/JetBrainsMonoNerdFont-Regular.ttf 
+cp ./.config/fonts/* ~/Library/Fonts/
 
 # Link iterm2 config
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
