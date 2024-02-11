39033022d8d21f802f628e450e8bab6d5ec3f34a
Sander Meij <sander@millerdigital.nl>
2024-02-11 17:04:43 +0100

    Simplify vi alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias ls="eza -a --icons";
 alias ll="eza -al --icons";
 alias lt="eza -a --tree --icons";
 
-alias vi=edit
+alias vi=nvim
 alias ts=tmux-session
 
 alias chrome-cors="open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir='/tmp/chrome_dev_test' --disable-web-security"
