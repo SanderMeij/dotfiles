7df7b3e1b5dac3d6d8dc42bbb49038554d81c503
Sander Meij <sander@millerdigital.nl>
2024-02-11 02:13:36 +0100

    Dimmer search highlight

--- .config/nvim/lua/user/one-monokai.lua
+++ .config/nvim/lua/user/one-monokai.lua
@@ -1,11 +1,18 @@
 local M = {
-  "cpea2506/one_monokai.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+    "cpea2506/one_monokai.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme 'one_monokai'
+    vim.cmd.colorscheme 'one_monokai'
+    require("one_monokai").setup({
+        themes = function(colors)
+            return {
+                Search = { bg = colors.dark_gray },
+            }
+        end,
+    })
 end
 
 return M
