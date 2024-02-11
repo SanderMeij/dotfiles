f77d6aeb2b03fdf82b901093dc2786ddd6b0367d
Sander Meij <sander@millerdigital.nl>
2024-02-11 02:42:57 +0100

    Fix git switch mapping

--- .config/nvim/lua/user/fugitive.lua
+++ .config/nvim/lua/user/fugitive.lua
@@ -1,16 +1,13 @@
 local M = {
     "tpope/vim-fugitive",
     event = "VeryLazy",
-    keys = {
-        {'<leader>Gs', '<cmd>vertical G<cr><cmd>vertical resize 40<cr>', { desc = '[G]it [S]tatus' }}
-    },
 }
 
 function M.config()
     local wk = require "which-key"
     wk.register {
         ["<leader>GP"] = { "<cmd>G push<CR>", "[G]it [P]ush" },
-        ["<leader>GS"] = { "<cmd>G switch<CR>", "[G]it [S]witch" },
+        ["<leader>GS"] = { ":G switch ", "[G]it [S]witch" },
         ["<leader>Gc"] = { "<cmd>G commit<CR>", "[G]it [c]ommit" },
         ["<leader>Gp"] = { "<cmd>G pull<CR>", "[G]it [p]ull" },
         ["<leader>Gs"] = { "<cmd>tab G<CR>", "[G]it [s]tatus" },
