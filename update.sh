74a2128cef48a4df6e254c7cddc07a3cc0478e25
Sander Meij <sander@millerdigital.nl>
2024-02-25 02:29:08 +0100

    Remove generated files

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ process_file() {
 
 export -f process_file
 
-# Find files and execute the function on each file
-find . -name "*.*.pkl" -type f -exec bash -c 'process_file "$0"' {} \;
-
 stow .
+
+# Find files and execute the function on each file
+find ~/.config -name "*.*.pkl" -type f -exec bash -c 'process_file "$0"' {} \;
