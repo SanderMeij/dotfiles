ada2f0f5c4a19444cc0454102b8e4a3c6bb749e1
Sander <sander@millerdigital.nl>
2024-04-30 17:29:41 +0200

    Fix tailwind tools warning

--- .config/nvim/lua/user/tailwind-tools.lua
+++ .config/nvim/lua/user/tailwind-tools.lua
@@ -1,9 +1,14 @@
 return {
-    "luckasRanarison/tailwind-tools.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {
-        conceal = {
-            enabled = false,
-        },
-    },
+	"luckasRanarison/tailwind-tools.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	opts = {
+		document_color = {
+			enabled = true,
+			kind = "background",
+			debounce = 200,
+		},
+		conceal = {
+			enabled = false,
+		},
+	},
 }
