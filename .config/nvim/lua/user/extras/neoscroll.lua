6a3b4b189e534c2451f8512eb397d1089486bcba
Sander <sander@millerdigital.nl>
2024-05-08 17:19:37 +0200

    Remove c-j and c-k from neoscroll

--- .config/nvim/lua/user/extras/neoscroll.lua
+++ .config/nvim/lua/user/extras/neoscroll.lua
@@ -3,14 +3,6 @@ local M = {
 }
 
 function M.config()
-  vim.cmd [[
-  nnoremap <C-j> <C-D>
-  vnoremap <C-j> <C-D>
-
-  nnoremap <C-k> <C-U>
-  vnoremap <C-k> <C-U>
-]]
-
   require("neoscroll").setup {
     -- All these keys will be mapped to their corresponding default scrolling animation
     mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb", "C-k", "C-j" },
@@ -28,8 +20,6 @@ function M.config()
   -- Syntax: t[keys] = {function, {function arguments}}
   t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
   t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
-  t["<C-k>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
-  t["<C-j>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
   t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "450" } }
   t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "450" } }
   t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
