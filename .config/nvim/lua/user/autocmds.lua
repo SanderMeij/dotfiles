b8dbd0edac1f4469a76b123820906bd4ea2496c7
Sander <sander@millerdigital.nl>
2024-05-13 17:36:56 +0200

    Add autoremove trailing whitespace

--- .config/nvim/lua/user/autocmds.lua
+++ .config/nvim/lua/user/autocmds.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  callback = function()
-    vim.cmd "set formatoptions-=cro"
-  end,
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+	end,
 })
 
 -- vim.api.nvim_create_autocmd({ "FileType" }, {
@@ -48,29 +48,35 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 -- })
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 40 }
-  end,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 40 })
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "markdown", "NeogitCommitMessage" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
+	pattern = { "gitcommit", "markdown", "NeogitCommitMessage" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "CursorHold" }, {
-  callback = function()
-    local status_ok, luasnip = pcall(require, "luasnip")
-    if not status_ok then
-      return
-    end
-    if luasnip.expand_or_jumpable() then
-      -- ask maintainer for option to make this silent
-      -- luasnip.unlink_current()
-      vim.cmd [[silent! lua require("luasnip").unlink_current()]]
-    end
-  end,
+	callback = function()
+		local status_ok, luasnip = pcall(require, "luasnip")
+		if not status_ok then
+			return
+		end
+		if luasnip.expand_or_jumpable() then
+			-- ask maintainer for option to make this silent
+			-- luasnip.unlink_current()
+			vim.cmd([[silent! lua require("luasnip").unlink_current()]])
+		end
+	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = { "*" },
+	command = [[%s/\s\+$//e]],
+})
+
